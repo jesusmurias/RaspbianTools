dc1b58fe6926c04fbdfec19bc989ab4465f59f3d
Jesus Murias <jesus.murias@outlook.com>
2024-03-15 13:33:52 +0100

    Nuevo

---
+++ amule.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+# Verificar si se proporcionan argumentos
+usage() {
+    echo "Use: "
+    echo "	$(basename $0) status"
+    echo "	$(basename $0) restart"
+    echo "	$(basename $0) stop"
+    echo "	$(basename $0) start"
+}
+
+if [ $# -eq 0 ]; then
+	ORDER="status"
+else
+	ORDER=$(echo $1 |  tr [:upper:] [:lower:])
+fi
+DAEMON="amule-daemon"
+
+
+case $ORDER in
+	"status") 
+		service $DAEMON status
+		exit 0
+		;;
+	"restart") 
+		service $DAEMON restart
+		exit 0
+		;;
+	"start") 
+		service $DAEMON start
+		exit 0
+		;;
+	"stop") 
+		service $DAEMON stop
+		exit 0
+		;;
+	
+	*)
+		echo "<$ORDER> option invalid"
+		usage
+		exit 1
+		;;
+esac
+
